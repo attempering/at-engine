a7ea4754446175380277cc6332ad9b9b3d5c88ef
3ki5tj <3ki5tj@gmail.com>
2023-11-11 01:35:32 +0800

    add the at.distr.bias unit test case

--- src/tests/unit_tests/distr/bias/test.c
+++ src/tests/unit_tests/distr/bias/test.c
@@ -3,6 +3,8 @@
 static void run() {
   at_t at[1];
   int i;
+  const double tol = 1e-5;
+  int err = 0;
 
   at__init(at, "at.cfg", NULL, 0);
 
@@ -13,6 +15,28 @@ static void run() {
     double w_deriv = at_distr__get_bias_w_deriv(at->distr, beta);
 
     printf("%g %g %g\n", beta, w_comb, w_deriv);
+
+    {
+      double temp = at__beta_to_temp(at, beta);
+      double w_comb2 = (temp - at->distr->bias->ref_temp)
+                     / (at->distr->bias->max_temp - at->distr->bias->ref_temp);
+      double w_deriv2 = - at->distr->bias->ref_temp
+                     / (at->distr->bias->max_temp - at->distr->bias->ref_temp);
+
+      if (fabs(w_comb - w_comb2) > tol) {
+        err += 1;
+      }
+
+      if (fabs(w_deriv - w_deriv2) > tol) {
+        err += 2;
+      }
+    }
+  }
+
+  if (err == 0) {
+    fprintf(stderr, "Passed.\n");
+  } else {
+    fprintf(stderr, "Failed.\n");
   }
 
   at__finish(at);
